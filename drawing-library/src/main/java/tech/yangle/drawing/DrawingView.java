f4addec37545772ab7714293c02e8b70bad6897a
yangle <mr.yang0229@foxmail.com>
2020-10-20 14:45:30 +0800

    逻辑优化调整

--- drawing-library/src/main/java/tech/yangle/drawing/DrawingView.java
+++ drawing-library/src/main/java/tech/yangle/drawing/DrawingView.java
@@ -26,8 +26,11 @@ public class DrawingView extends View {
     private BasePen mPaint;
     private BasePen mTriPaint;
     private Path mPath;
+    private Path mTriPath;
     private float mLastX;
+    private float mTriLastX;
     private float mLastY;
+    private float mTriLastY;
     private Bitmap mBufferBitmap;
     private Canvas mBufferCanvas;
     // 是否为三方绘制
@@ -49,6 +52,12 @@ public class DrawingView extends View {
         super(context, attrs, defStyleAttr);
     }
 
+    /**
+     * 初始化
+     *
+     * @param width  画布宽度
+     * @param height 画布高度
+     */
     public void init(int width, int height) {
         // 不使用硬件加速
         setLayerType(View.LAYER_TYPE_SOFTWARE, null);
@@ -56,6 +65,7 @@ public class DrawingView extends View {
         // 双缓存机制
         mBufferBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_4444);
         mBufferCanvas = new Canvas(mBufferBitmap);
+        mBufferCanvas.drawColor(Color.TRANSPARENT);
 
         // 默认画笔
         mPaint = new StandardPen(getContext());
@@ -63,6 +73,8 @@ public class DrawingView extends View {
         mTriPaint = new StandardPen(getContext());
         // 笔迹路径
         mPath = new Path();
+        // 外部调用，笔迹路径
+        mTriPath = new Path();
     }
 
     @Override
@@ -72,39 +84,63 @@ public class DrawingView extends View {
             canvas.drawBitmap(mBufferBitmap, 0, 0, null);
         }
         // ACTION_MOVE时，将笔迹临时绘制在画布上
-        canvas.drawPath(mPath, mIsTriDraw ? mTriPaint : mPaint);
+        if (mPath != null) {
+            canvas.drawPath(mIsTriDraw ? mTriPath : mPath, mIsTriDraw ? mTriPaint : mPaint);
+        }
     }
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        if (!mIsCanDraw) {
+        if (mPath == null) {
             return true;
         }
         mIsTriDraw = event.getMetaState() == 100;
-        if (mTouchListener != null) {
-            mTouchListener.onTouch(event);
+        if (!mIsTriDraw) {
+            if (!mIsCanDraw) {
+                return true;
+            }
+            if (mTouchListener != null) {
+                mTouchListener.onTouch(event);
+            }
         }
         float x = event.getX();
         float y = event.getY();
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                mLastX = x;
-                mLastY = y;
-                mPath.moveTo(x, y);
+                if (mIsTriDraw) {
+                    mTriLastX = x;
+                    mTriLastY = y;
+                    mTriPath.moveTo(x, y);
+                } else {
+                    mLastX = x;
+                    mLastY = y;
+                    mPath.moveTo(x, y);
+                }
                 break;
 
             case MotionEvent.ACTION_MOVE:
-                mPath.quadTo(mLastX, mLastY, (x + mLastX) / 2, (y + mLastY) / 2);
-                mLastX = x;
-                mLastY = y;
+                if (mIsTriDraw) {
+                    mTriPath.quadTo(mTriLastX, mTriLastY, (x + mTriLastX) / 2, (y + mTriLastY) / 2);
+                    mTriLastX = x;
+                    mTriLastY = y;
+                } else {
+                    mPath.quadTo(mLastX, mLastY, (x + mLastX) / 2, (y + mLastY) / 2);
+                    mLastX = x;
+                    mLastY = y;
+                }
                 invalidate();
                 break;
 
             case MotionEvent.ACTION_UP:
                 // ACTION_UP时，将当前一笔的笔迹，绘制到缓存画布上
-                mBufferCanvas.drawPath(mPath, mIsTriDraw ? mTriPaint : mPaint);
-                mPath.reset();
+                if (mIsTriDraw) {
+                    mBufferCanvas.drawPath(mTriPath, mTriPaint);
+                    mTriPath.reset();
+                } else {
+                    mBufferCanvas.drawPath(mPath, mPaint);
+                    mPath.reset();
+                }
                 invalidate();
                 break;
         }
@@ -189,17 +225,6 @@ public class DrawingView extends View {
     }
 
     /**
-     * 设置画笔透明度
-     *
-     * @param alpha 画笔透明度，0..255
-     */
-    public void setPenAlpha(int alpha) {
-        if (mPaint != null) {
-            mPaint.setAlpha(alpha);
-        }
-    }
-
-    /**
      * 设置三方画笔颜色
      *
      * @param penColor 画笔颜色
@@ -207,6 +232,28 @@ public class DrawingView extends View {
     public void setTriPenColor(int penColor) {
         if (mTriPaint != null) {
             mTriPaint.setColor(penColor);
+        }
+    }
+
+    /**
+     * 设置画笔透明度
+     *
+     * @param penAlpha 画笔透明度，0..255
+     */
+    public void setPenAlpha(int penAlpha) {
+        if (mPaint != null) {
+            mPaint.setAlpha(penAlpha);
+        }
+    }
+
+    /**
+     * 设置三方画笔透明度
+     *
+     * @param penAlpha 画笔透明度，0..255
+     */
+    public void setTriPenAlpha(int penAlpha) {
+        if (mTriPaint != null) {
+            mTriPaint.setAlpha(penAlpha);
         }
     }
 
